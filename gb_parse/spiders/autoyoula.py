c393256f90d0895eb781d94b1ef6a84fe0d8463b
shilo <shi095@gmail.com>
2021-03-08 21:32:53 +0300

    lesson 4 - HW

---
+++ gb_parse/spiders/autoyoula.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class AutoyoulaSpider(scrapy.Spider):
+    name = 'autoyoula'
+    allowed_domains = ['auto.youla.ru']
+    start_urls = ['http://auto.youla.ru/']
+
+    def parse(self, response):
+        pass
