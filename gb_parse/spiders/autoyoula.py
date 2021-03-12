38a05d44535e1716cb90f9149393d2463aa03188
shilo <shi095@gmail.com>
2021-03-12 22:57:34 +0300

    lesson 4 - HW

--- gb_parse/spiders/autoyoula.py
+++ gb_parse/spiders/autoyoula.py
@@ -11,10 +11,23 @@ class AutoyoulaSpider(scrapy.Spider):
         'brands':".TransportMainFilters_brandsList__2tIkv .ColumnItemList_container__5gTrc a.blackLink",
         'pagination':"a.Paginator_button__u1e7D",
         'car':".SerpSnippet_titleWrapper__38bZM a.SerpSnippet_name__3F7Yu",
+    }
+    data_question = {
+        'title': lambda response: response.css(".AdvertCard_advertTitle__1S1Ak::text").extract_first(),
+        'photos': lambda response: [item for item in response.css(".PhotoGallery_photo__36e_r img::attr(src)").extract()],
+        'characteristics':lambda response:[
+            {'name': item.css('.AdvertSpecs_label__2JHnS::text').get(),
+             'value': item.css('.AdvertSpecs_data__xK2Qx::text').get()
+                      or item.css('.AdvertSpecs_data__xK2Qx a::text').get()
+             }
+        for item in response.css('.AdvertCard_specs__2FEHc .AdvertSpecs_row__ljPcX')
+        ],
+        'description': lambda response: response.css('.AdvertCard_descriptionInner__KnuRi::text').extract(),
+        'author_url':"",
+        'phone':""
 
 
     }
-
     def _get_follow(self, response, select_str, callback, **kwargs):
         for a in response.css(select_str):
             link = a.attrib.get("href")
@@ -38,5 +51,11 @@ class AutoyoulaSpider(scrapy.Spider):
 # Обработан урок 4. 09.03.21 будет дополнено выполненным домашним заданием
     def car_parse(self,response):
         print(1)
+ #       for key, selector in self.data_question.items():
+ #           try:
+#                data[key] = selector(response)
+#            except (ValueError, AttributeError):
+#                continue
+# дописать!!!!
 
 
